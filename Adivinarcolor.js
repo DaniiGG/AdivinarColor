2e796ca1b652d09b34fddce446302e3fb20c7a0a
Daniel Escobar <daniescomoli@gmail.com>
2023-10-25 13:45:26 +0200

    css hecho

--- Adivinarcolor.js
+++ Adivinarcolor.js
@@ -1,7 +1,47 @@
-function getRandomColor() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r}, ${g}, ${b})`;
+window.onload = function () {
+    
+    const muestra = document.getElementById('muestra');
+    const opciones = document.querySelectorAll('.opcion');
+
+    function getRandomColor() {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+
+    function iniciarJuego() {
+        
+        const colorCorrecto = getRandomColor();
+        muestra.style.backgroundColor = colorCorrecto;
+
+        
+        const colores = [colorCorrecto, getRandomColor(), getRandomColor()];
+        colores.sort(() => Math.random() - 0.5);
+
+        
+        opciones.forEach((opcion, index) => {
+            opcion.style.backgroundColor = colores[index];
+            opcion.addEventListener('click', () => verificarAdivinanza(colores[index], opcion));
+        });
+    
+
+    function verificarAdivinanza(color, opcion) {
+        if (color === colorCorrecto) {
+            
+            opciones.forEach((opcion) => {
+                opcion.style.backgroundColor = colorCorrecto;
+               
+            });
+            setTimeout(() => {
+                window.location.reload();
+            }, 2000);
+        } else {
+            opcion.style.backgroundColor = 'transparent';  
+        }
+    }
 }
-Colorrandom=getRandomColor();
+iniciarJuego();
+
+    
+};
